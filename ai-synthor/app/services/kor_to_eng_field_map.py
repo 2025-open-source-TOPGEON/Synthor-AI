5c72ac0826c9ac197d3b58501fc7fcc84fe9782b
irisiris0428 <irisiris0428@naver.com>
2025-08-04 06:24:44 +0900

    feat: 한글 필드명 매핑 파일 정리 추가

---
+++ ai-synthor/app/services/kor_to_eng_field_map.py
@@ -0,0 +1,210 @@
+KOR_TO_ENG_FIELD = {
+    # korean_full_name
+    "전체 이름": "korean_full_name",
+    "이름 전체": "korean_full_name",
+    "성명": "korean_full_name",
+    "풀네임": "korean_full_name",
+    "전성 이름": "korean_full_name",
+    "이름": "korean_full_name",
+    "이름 전체": "korean_full_name",
+    "이름 전부": "korean_full_name",
+    "이름 풀": "korean_full_name",
+    "이름(전체)": "korean_full_name",
+    "이름(풀)": "korean_full_name",
+    "이름 전체명": "korean_full_name",
+    "이름 풀네임": "korean_full_name",
+    # korean_last_name
+    "성": "korean_last_name",
+    "성씨": "korean_last_name",
+    "이씨": "korean_last_name",
+    "김씨": "korean_last_name",
+    "박씨": "korean_last_name",
+    "최씨": "korean_last_name",
+    "이 성": "korean_last_name",
+    "김 성": "korean_last_name",
+    "박 성": "korean_last_name",
+    "최 성": "korean_last_name",
+    "성(이)": "korean_last_name",
+    "성(김)": "korean_last_name",
+    "성(박)": "korean_last_name",
+    "성(최)": "korean_last_name",
+    # korean_first_name
+    "이름(이)": "korean_first_name",
+    "이름(김)": "korean_first_name",
+    "이름(박)": "korean_first_name",
+    "이름(최)": "korean_first_name",
+    "이름만": "korean_first_name",
+    "이름(한글)": "korean_first_name",
+    "이름(영문)": "korean_first_name",
+    # korean_address
+    "주소": "korean_address",
+    "전체 주소": "korean_address",
+    "집 주소": "korean_address",
+    "거주지": "korean_address",
+    "거주 주소": "korean_address",
+    "거주지 주소": "korean_address",
+    "거주지 전체": "korean_address",
+    # korean_street_address
+    "도로명 주소": "korean_street_address",
+    "도로 주소": "korean_street_address",
+    "도로명": "korean_street_address",
+    "도로 전체": "korean_street_address",
+    "도로명 전체": "korean_street_address",
+    "도로명주소": "korean_street_address",
+    # korean_city
+    "도시": "korean_city",
+    "도시 이름": "korean_city",
+    "사는 도시": "korean_city",
+    "거주 도시": "korean_city",
+    "시": "korean_city",
+    "시 이름": "korean_city",
+    "도시 이름(한글)": "korean_city",
+    # korean_state
+    "시도": "korean_state",
+    "도": "korean_state",
+    "시/도": "korean_state",
+    "시 도": "korean_state",
+    "도 이름": "korean_state",
+    "시 이름": "korean_state",
+    # korean_country
+    "국가": "korean_country",
+    "나라": "korean_country",
+    "거주 국가": "korean_country",
+    "사는 나라": "korean_country",
+    # korean_postal_code
+    "우편번호": "korean_postal_code",
+    "우편 코드": "korean_postal_code",
+    "우편 번호": "korean_postal_code",
+    "우편코드": "korean_postal_code",
+    # korean_phone
+    "전화번호": "korean_phone",
+    "휴대폰 번호": "korean_phone",
+    "핸드폰 번호": "korean_phone",
+    "연락처": "korean_phone",
+    "휴대폰": "korean_phone",
+    "핸드폰": "korean_phone",
+    "전화": "korean_phone",
+    # korean_company_name
+    "회사명": "korean_company_name",
+    "회사 이름": "korean_company_name",
+    "근무처": "korean_company_name",
+    "근무 회사": "korean_company_name",
+    # korean_job_title
+    "직책": "korean_job_title",
+    "직위": "korean_job_title",
+    "포지션": "korean_job_title",
+    "직무": "korean_job_title",
+    # korean_department_corporate
+    "부서": "korean_department_corporate",
+    "소속": "korean_department_corporate",
+    "소속 부서": "korean_department_corporate",
+    "소속팀": "korean_department_corporate",
+    # korean_product_name
+    "제품명": "korean_product_name",
+    "상품명": "korean_product_name",
+    "제품 이름": "korean_product_name",
+    "상품 이름": "korean_product_name",
+    # korean_product_category
+    "제품 카테고리": "korean_product_category",
+    "상품 카테고리": "korean_product_category",
+    "제품 종류": "korean_product_category",
+    "상품 종류": "korean_product_category",
+    # korean_catch_phrase
+    "캐치프레이즈": "korean_catch_phrase",
+    "슬로건": "korean_catch_phrase",
+    "표어": "korean_catch_phrase",
+    # korean_product_description
+    "제품 설명": "korean_product_description",
+    "상품 설명": "korean_product_description",
+    "제품 소개": "korean_product_description",
+    "상품 소개": "korean_product_description",
+    # korean_language
+    "언어": "korean_language",
+    "사용 언어": "korean_language",
+    "쓰는 언어": "korean_language",
+    # korean_color
+    "색상": "korean_color",
+    "컬러": "korean_color",
+    "색": "korean_color",
+    # username
+    "사용자명": "username",
+    "유저명": "username",
+    "계정명": "username",
+    "계정 이름": "username",
+    "유저 이름": "username",
+    # password
+    "비밀번호": "password",
+    "패스워드": "password",
+    "비번": "password",
+    # email_address
+    "이메일": "email_address",
+    "이메일 주소": "email_address",
+    "메일": "email_address",
+    "메일 주소": "email_address",
+    # domain_name
+    "도메인": "domain_name",
+    "도메인 이름": "domain_name",
+    # url
+    "URL": "url",
+    "주소(URL)": "url",
+    "링크": "url",
+    "웹주소": "url",
+    # mac_address
+    "MAC 주소": "mac_address",
+    # ip_v4_address
+    "IPv4 주소": "ip_v4_address",
+    # ip_v6_address
+    "IPv6 주소": "ip_v6_address",
+    # user_agent
+    "유저 에이전트": "user_agent",
+    # avatar
+    "아바타": "avatar",
+    "프로필 사진": "avatar",
+    "프로필 이미지": "avatar",
+    # app_name
+    "앱 이름": "app_name",
+    "어플 이름": "app_name",
+    # app_version
+    "버전": "app_version",
+    "앱 버전": "app_version",
+    # device_model
+    "기기 모델": "device_model",
+    # device_brand
+    "기기 브랜드": "device_brand",
+    # device_os
+    "OS": "device_os",
+    "운영체제": "device_os",
+    # credit_card_number
+    "카드번호": "credit_card_number",
+    "신용카드 번호": "credit_card_number",
+    # credit_card_type
+    "카드 종류": "credit_card_type",
+    "카드 타입": "credit_card_type",
+    "VISA MASTER 종류": "credit_card_type",
+    # currency
+    "통화": "currency",
+    "화폐 단위": "currency",
+    # iban
+    "IBAN": "iban",
+    "IBAN 코드": "iban",
+    # swift_bic
+    "SWIFT": "swift_bic",
+    "SWIFT 코드": "swift_bic",
+    # paragraphs
+    "문단": "paragraphs",
+    "단락": "paragraphs",
+    "설명 글": "paragraphs",
+    # datetime
+    "날짜": "datetime",
+    "일시": "datetime",
+    # time
+    "시간": "time",
+    # latitude
+    "위도": "latitude",
+    # longitude
+    "경도": "longitude",
+    # number_between_1_100
+    "숫자": "number_between_1_100",
+    "1과 100 사이 숫자": "number_between_1_100",
+    "1~100 숫자": "number_between_1_100",
+}
